d0448af532ccaf07f767fd85dbbabcceafeafe28
enrico bellanti <67062039+enrico-bellanti@users.noreply.github.com>
2022-06-22 12:46:59 +0200

    database connection settings

--- be/index.js
+++ be/index.js
@@ -2,3 +2,20 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30 mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30 mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://full-stack-mern:Be190686!@cluster0.8m52odk.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
