936e9865abb2bacb5fa87cf47d1fb54b3dc8e676
enrico bellanti <67062039+enrico-bellanti@users.noreply.github.com>
2022-07-06 17:55:09 +0200

    google auth

---
+++ fe/src/components/Home/Home.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { Container, Grow, Grid } from "@material-ui/core";
+//components
+import Form from "../Form/Form";
+import Posts from "../Posts/Posts";
+
+//hooks
+import { useDispatch } from "react-redux";
+
+//styles
+import useStyles from "./styles";
+
+//actions
+import { getPosts } from "../../actions/posts";
+
+export const Home = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+  return (
+    <Grow in>
+      <Container>
+        <Grid
+          className={classes.mainContainer}
+          container
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
