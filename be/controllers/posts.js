8e1cc4c5d513bb1407b732053474a8998d896e01
enrico bellanti <67062039+enrico-bellanti@users.noreply.github.com>
2022-07-06 12:06:59 +0200

    refactoring env strings action types

--- be/controllers/posts.js
+++ be/controllers/posts.js
@@ -50,14 +50,14 @@ export const deletePost = async (req, res) => {
 };
 
 export const likePost = async (req, res) => {
-  const { id } = req.params;
+  const { id: _id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(_id))
     return res.status(404).send("No post with that id");
 
-  const post = await PostMessage.findById(id);
+  const post = await PostMessage.findById(_id);
   const updatedPost = await PostMessage.findByIdAndUpdate(
-    id,
+    _id,
     {
       likeCount: post.likeCount + 1,
     },
--- be/index.js
+++ be/index.js
@@ -2,22 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30 mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30 mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://full-stack-mern:Be190686!@cluster0.8m52odk.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
